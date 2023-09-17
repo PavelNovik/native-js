1bd6a6279b59a4954df971ac4a98079b0744ac12
Pavel Novik <brn2befree@gmail.com>
2023-09-17 18:05:44 +0300

    about test

---
+++ src/01-hello-test/01.test.ts
@@ -0,0 +1,8 @@
+import {sum} from "./01";
+
+test('Sum should be correct', ()=> {
+    const a = 1;
+    const b = 2;
+    const c = 3;
+    const result = sum(a, b)
+})
---
+++ src/01-hello-test/01.ts
@@ -0,0 +1,10 @@
+const sentense = "Hello my friend!"
+
+
+export function sum(a: number, b: number) {
+    return a + b
+}
+export function mult(a: number, b: number) {
+    return a * b
+}
+export function splitIntoWords (sentense: string) {}
