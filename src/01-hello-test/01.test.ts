6561a8cb23df4d772d1057c43d2d0130361b641f
Pavel Novik <brn2befree@gmail.com>
2023-09-18 09:31:56 +0300

    Add tests & debugging

--- src/01-hello-test/01.test.ts
+++ src/01-hello-test/01.test.ts
@@ -1,11 +1,21 @@
 import {mult, splitIntoWords, sum} from "./01";
 
+// data
+let a: number;
+let b: number;
+let c: number;
+
+beforeEach(() => {
+    a = 1;
+    b = 2;
+    c = 3;
+})
 test('Sum should be correct', ()=> {
 
     // data
-    const a = 1;
-    const b = 2;
-    const c = 3;
+    // const a = 1;
+    // const b = 2;
+    // const c = 3;
 
     // action
     const result1 = sum(a, b)
@@ -19,9 +29,9 @@ test('Sum should be correct', ()=> {
 
 test('Multiply should be correct', () => {
     // data
-    const a = 1;
-    const b = 2;
-    const c = 3;
+    // const a = 1;
+    // const b = 2;
+    // const c = 3;
 
     // action
     const result1 = mult(a, b)
@@ -35,13 +45,22 @@ test('Multiply should be correct', () => {
 test('Splitting into words should be correct', ()=> {
     // data
     const sentence1 = "Hello my friend!";
-    const sentence2 = "JS - programming language."
+    const sentence2 = "JS - the best programming  language."
     // action
     const result1 = splitIntoWords(sentence1);
     const result2 = splitIntoWords(sentence2);
 
     // expect result
-    expect(result1).toBe(2)
-    expect(result2).toBe(6)
+    expect(result1.length).toBe(3)
+    expect(result1[0]).toBe("hello")
+    expect(result1[1]).toBe("my")
+    expect(result1[2]).toBe("friend")
+
+    expect(result2.length).toBe(5)
+    expect(result2[0]).toBe("js")
+    expect(result2[1]).toBe("the")
+    expect(result2[2]).toBe("best")
+    expect(result2[3]).toBe("programming")
+    expect(result2[4]).toBe("language")
 
 })
--- src/01-hello-test/01.ts
+++ src/01-hello-test/01.ts
@@ -4,7 +4,12 @@ const sentence = "Hello my friend!"
 export function sum(a: number, b: number) {
     return a + b
 }
+
 export function mult(a: number, b: number) {
     return a * b
 }
-export function splitIntoWords (sentence: string) {}
+
+export function splitIntoWords(sentence: string) {
+    const words = sentence.toLowerCase().split(" ")
+        return words.filter(s => s !== '' && s !== '-').map(str => str.replace("!", "").replace(".", ''))
+}
